fcf86c5d18aed7a8e0793b61110e03a8f1a9a648
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-13 16:58:20 +0530

    Update 1.cpp

--- Day-4/1.cpp
+++ Day-4/1.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int num, digits=0
-  cout>>"Enter a number: ">>endl;
-  cin<<num;
-  
+  int num, digits=0;
+  cout<<"Enter a number: "<<endl;
+  cin>>num;
+  if(num==0){
+    cout<<"Number is 0"<<endl;
+  }
+  else{
+    while(num>0){
+        int temp=num%10;
+        if(temp%2==0){
+            digits++;
+            num=num/10;
+        }
+        else{
+            num=num/10;
+        }
+    }
+  }
+  cout<<"Number of even digits: "<<digits<<endl;
+}
