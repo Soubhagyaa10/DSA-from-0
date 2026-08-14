9f152eebbb767b8199e8c33f92b00dd7bf7cd402
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-14 23:59:35 +0530

    Add files via upload

--- Day-5/1.cpp
+++ Day-5/1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+    int num, num2, fact = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+    num2=num;
+    if(num==0){
+        fact = 1;
+    } 
+    while(num>0){
+        fact *= num;
+        num--;
+    }
+    cout << "Factorial of " << num2 << " is " << fact << endl;
     return 0;
 }
