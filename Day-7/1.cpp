562fb738c7332700cc364294d024f135f1ca16ad
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-16 13:07:40 +0530

    Add files via upload

---
+++ Day-7/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,min;
+    cout<<"Enter a number: ";
+    cin>>num;
+    min=num%10;
+    while(num>0){;
+        if(num%10<min)
+        {
+            min=num%10;
+        }        
+        num=num/10;
+    }
+    cout<<"The smallest digit is: "<<min<<endl;
+}
