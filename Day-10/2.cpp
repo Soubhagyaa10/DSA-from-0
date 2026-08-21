45998b22267ccdf52b22e7952b83443cf7458db8
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-22 01:37:56 +0530

    Add files via upload

--- Day-10/2.cpp
+++ Day-10/2.cpp
@@ -4,8 +4,8 @@ int main(){
     int i,j,k,rows;
     cout<<"Enter number of rows: "<<endl;
     cin>>rows;
-    for(i=1;i<=rows;i++){
-        for(j=rows;j>0;j--){
+    for(i=rows;i>0;i--){
+        for(j=1;j<i;j++){
             cout<<" ";
         }
         for(k=1;k<=i;k++){
---
+++ Day-10/3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,rows;
+    cout<<"Enter number of rows: "<<endl;
+    cin>>rows;
+    for(i=1;i<=rows;i++){
+        for (j=1;j<;j++){
+
+        }
+        cout<<endl;}
+}
