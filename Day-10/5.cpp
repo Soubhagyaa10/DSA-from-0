c126a7b18574db4f4d3fb37f34422d2c240ca0ed
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-23 23:33:24 +0530

    Add files via upload

--- Day-10/5.cpp
+++ Day-10/5.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 int main(){
     int i,j,k,rows;
-    cout<<"Enter number";
+    cout<<"Enter odd number:";
     cin>>rows;
-    for(i=1;i<=rows;i++){
-        for(j=i;j<rows;j++){
+    for(i=1;i<=(rows/2)+1;i++){
+        for(j=i;j<(rows/2)+1;j++){
             cout<<" ";
         }
-        for(k=1;k<=i;k++){
+        for(k=1;k<=2*i-1;k++){
             cout<<"*";
         }
         cout<<endl;
     }
-    for(i=1;i<rows;i++){
-        for(j=1;j<i;j++){
+    for(i=(rows/2)+2; i<=rows;i++){
+        for(j=rows;j>=i;j--){
             cout<<" ";
         }
-        for(k=rows-1;k>=1;k--){
+        for(k=; k>i; k++){
             cout<<"*";
         }
         cout<<endl;
