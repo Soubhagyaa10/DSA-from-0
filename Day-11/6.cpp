77711f8d7bbce48f38abe046e8e3925546a652ca
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-24 23:28:01 +0530

    Add files via upload

--- Day-11/6.cpp
+++ Day-11/6.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 int main(){
     int arr[5]={1,21,11,40,15};
-    int i, max=arr[0];
+    int i, max1, max2;
+    if(arr[0]>arr[1]){
+        max1=arr[0],max2=arr[1];
+    }
+    else{
+        max1=arr[1],max2=arr[0];
+    }
     for(i=0;i<5;i++){
-        if(arr[i]>max){
-            max=arr[i];
+        if(arr[i]>max2 && arr[i]<max1){
+            max2=arr[i];
+        }
+        else if(arr[i]>max1){
+            max2=max1;
+            max1=arr[i];
         }
     }
-    cout<<"Largest number = "<<max<<endl;
+    cout<<"Second Largest number = "<<max2<<endl;
 }
