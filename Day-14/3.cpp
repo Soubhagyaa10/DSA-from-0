7ce5064497b5a99ed6f0918f30170e66a6beabcd
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-27 00:02:29 +0530

    Add files via upload

---
+++ Day-14/3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,3,2,4,0};
+    int temp;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<4;j++){
+            if (arr[j]<arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
