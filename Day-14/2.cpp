b0ee252d01d11d96acfbd94f092ae3fdc9c9759b
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-27 00:01:32 +0530

    Add files via upload

--- Day-14/2.cpp
+++ Day-14/2.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5]={1,3,2,4,5};
+    int arr[5]={1,3,2,4,0};
     int temp;
-    temp=arr[1];
-    arr[1]=arr[2];
-    arr[2]=temp;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<4;j++){
+            if (arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            
+        }
+    }
     for(int i=0;i<5;i++){
         cout<<arr[i]<<" ";
     }
