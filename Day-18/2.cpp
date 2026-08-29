f892b2540ed5e9483785e5eca85e16a2910e8302
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-08-30 00:01:41 +0530

    Add files via upload

--- Day-18/2.cpp
+++ Day-18/2.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    
+    int arr[6]={1,5,7,-1,5,3};
+    int target=6,pairs=0;
+    for(int i=0;i<6;i++){
+        for(int j=6;j>i;j--){
+            if(arr[i]+arr[j]==target){
+                pairs++;
+            }
+        }
+    }
+    cout<<"Number of pairs= " <<pairs<<endl;
     return 0;
 }
