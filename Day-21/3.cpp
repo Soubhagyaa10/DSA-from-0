78b5cb029b03dc7b509af2324bcd1cf288ab5ef3
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-09-04 21:56:51 +0530

    Add files via upload

---
+++ Day-21/3.cpp
@@ -0,0 +1,16 @@
+//Find the longest consecutive sequence
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[7] = {100, 4, 200, 1, 3, 2, 5};
+    int longestSequence = 0;
+    for(int i=0; i<7; i++){
+        for(int j=1; j<7; j++){
+            if(){
+                longestSequence++;
+            }
+        }
+        cout<<"Longest consecutive sequence is: "<<longestSequence<<endl;
+        longestSequence = 0;
+    }
+}
