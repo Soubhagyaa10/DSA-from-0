d9a9927614ef1334b4b20c61c7da4c1af01a0a60
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-09-10 01:28:48 +0530

    Add files via upload

--- Day-23/3.cpp
+++ Day-23/3.cpp
@@ -2,7 +2,18 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int count=1,longest=0;
     int arr[10] = {1, 1, 2, 2, 2, 3, 3, 1, 1, 1};
-    
+    for(int i=0;i<9;i++){
+        if(arr[i]==arr[i+1]){
+            count++;
+        }
+        else{
+            count=1;}
+        if(count>longest){
+            longest=count;
+        }
+    }
+    cout<<"Longest equal consecutive sequence= "<<longest;
     return 0;
 }
