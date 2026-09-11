6d603f79cb83f4b40c754289a00c49e0c6287089
SOUBHAGYA <soubhagyamahajan10@gmail.com>
2026-09-12 02:38:07 +0530

    Add files via upload

--- Day-25/1.cpp
+++ Day-25/1.cpp
@@ -2,7 +2,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    arr = {0, 1, 0, 0, 1, 1};
-    
+    int arr[6] = {0, 1, 0, 1, 1, 1};
+    int zero=0,one=0, longest=0;
+    for(int i=0;i<6;i++){
+        zero=0,one=0;
+        for(int j=i;j<6;j++){
+            if(arr[j]==0)
+                zero++;
+            else if(arr[j]==1)
+                one++;     
+            if(zero==one && j-i+1>longest){
+                longest = j-i+1;
+            }       
+        }
+    }
+    cout<<"Length of longest subarray with equal 0s and 1s is "<<longest;
     return 0;
 }
